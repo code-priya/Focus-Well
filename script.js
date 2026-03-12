fcec0be2ed266b4da20ac12433303b6c0b920bf3
Priyamvada Singh <priyamvadasingh0202@gmail.com>
2026-03-12 16:39:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+const ctx = document.getElementById('stressChart');
 
-function showMessage() {
-  alert("Your GitHub Pages website is working!");
+new Chart(ctx, {
+type: 'bar',
+data: {
+labels: ['Low Stress','Moderate Stress','High Stress'],
+
+datasets: [{
+label: 'Number of Students',
+data: [300,1200,500],
+
+borderWidth: 1
+}]
+},
+
+options: {
+responsive:true,
+
+plugins:{
+legend:{
+display:false
 }
+},
+
+scales:{
+y:{
+beginAtZero:true
+}
+}
+}
+});
